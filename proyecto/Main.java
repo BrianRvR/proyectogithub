4805bf4b24fbfb8dba19298d7901362202ae95ae
BrianRvR <brianrvr12@gmail.com>
2023-05-17 23:32:40 +0200

    Añadido script que utiliza la función de media

---
+++ proyecto/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+import java.util.Arrays;
+import java.util.List;
+import lib.matlib.MatLib;
+
+/**
+ *
+ * @author brian
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<Double> nums = Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0);
+        System.out.println(MatLib.media(nums));
+    }
+}
